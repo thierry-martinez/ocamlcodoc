7ea0c99523ed44b556ff203f67da1e66c0fdc99f
Thierry Martinez <thierry.martinez@inria.fr>
2019-01-21 03:08:31 +0100

    Fix opam file name

--- release.sh
+++ release.sh
@@ -24,7 +24,7 @@ git checkout -b "$branch"
 repo="packages/ocamlcodoc/ocamlcodoc.$version"
 mkdir -p "$repo"
 opamfile="$repo/opam"
-cp ~/ocamlcodoc/opam "$opamfile"
+cp ~/ocamlcodoc/ocamlcodoc.opam "$opamfile"
 cat >>$opamfile <<EOF
 url {
   src: "$url"
