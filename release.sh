1350b17c175082079c3386cd5475171c2c676c71
Thierry Martinez <thierry.martinez@inria.fr>
2019-01-21 20:39:52 +0100

    Three number versioning
    Suggestion from Marcello Seri.
    
    Release script updated to push to the same branch (v1) of
    opam-repository. CI updated to trigger deploy on release commit
    messages.

--- release.sh
+++ release.sh
@@ -2,29 +2,28 @@
 set -e
 version="`cat VERSION`"
 tagname="v$version"
-commit="`git rev-parse HEAD`"
 git config --global user.email "Thierry.Martinez@inria.fr"
 git config --global user.name "Thierry Martinez"
-cd ~/ocamlcodoc
-git pull origin master
-if [[ "`git rev-parse HEAD`" != "$commit" ]]; then
-    echo "Too recent commit!"
-    exit 1
-fi
-git tag -a "$tagname" -m "Version $version"
-git push origin "$tagname"
+current_dir="`pwd`"
+git log -1 --format=%B >commit_message
+git tag -f -a "$tagname" -m "Version $version"
+git push -f origin "$tagname"
 archive="ocamlcodoc-$tagname.tar.gz"
 url="https://gitlab.inria.fr/tmartine/ocamlcodoc/-/archive/$tagname/$archive"
 wget "$url"
 md5=`md5sum "$archive" | cut -d " " -f 1`
 cd ~/opam-repository
 git pull origin master
-branch="ocamlcodoc.$version"
-git checkout -b "$branch"
+if [[ "$version" = 1.0.0 ]]; then
+    branch="ocamlcodoc.1"
+else
+    branch="ocamlcodoc.$version"
+fi
+git checkout -B "$branch"
 repo="packages/ocamlcodoc/ocamlcodoc.$version"
 mkdir -p "$repo"
 opamfile="$repo/opam"
-cp ~/ocamlcodoc/ocamlcodoc.opam "$opamfile"
+cp $current_dir/ocamlcodoc.opam "$opamfile"
 cat >>$opamfile <<EOF
 url {
   src: "$url"
@@ -32,6 +31,6 @@ url {
 }
 EOF
 git add "$opamfile"
-git commit -m "ocamlcodoc.$version"
+git commit -F "$current_dir/commit_message"
 git push perso "$branch"
 git checkout master
