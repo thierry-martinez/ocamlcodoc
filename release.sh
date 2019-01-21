6ab4ba92d669fb6a2a5139f2dcd9e576b48393b5
Thierry Martinez <thierry.martinez@inria.fr>
2019-01-21 01:30:07 +0100

    Automate release process

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+version="`cat VERSION`"
+tagname="v$version"
+git tag -a "$tagname" -m "Version $version"
+git push origin "$tagname"
+archive="ocamlcodoc-$tagname.tar.gz"
+url= "https://gitlab.inria.fr/tmartine/ocamlcodoc/-/archive/$tagname/$archive"
+wget "$url"
+md5=`md5sum "$archive" | cut -d " " -f 1`
+cd ~/opam-repository
+git pull origin master
+branch="ocamlcodoc.$version"
+git checkout -b "$branch"
+repo="packages/ocamlcodoc/ocamlcodoc.$version"
+mkdir -p "$repo"
+opamfile="$repo/opam"
+cp opam "$opamfile"
+cat >>$opamfile <<EOF
+url {
+  src: "$url"
+  checksum: "md5=$md5"
+}
+EOF
+git add "$opamfile"
+git commit -m "ocamlcodoc.$version"
+git push perso "$branch"
+git checkout master
